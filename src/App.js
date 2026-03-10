a21b4ca451245cb2367d42ddc2d53f511ae9cf0a
Dharshini A T <atdharshini709@gmail.com>
2026-03-10 13:12:18 +0530

    remove unwanted variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import "./App.css";
 
-import Header from './components/Header'
+import Header from "./components/Header";
 
 function App() {
-  const payment =
-    "You have finished your payment succcesfuly and then you want to wait for a couple of days";
+  
+
 
   return (
     <div>
       <Routes>
-      <Header />
+        <Header />
       </Routes>
     </div>
   );
