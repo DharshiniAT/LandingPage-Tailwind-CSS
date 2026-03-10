8c4a527a21448ea07fc538a2b0cc7f945647b4d0
Dharshini A T <atdharshini709@gmail.com>
2026-03-10 13:15:08 +0530

    remove unwanted variables

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import { Routes } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/Header";
 
 function App() {
-  
-
-
   return (
     <div>
-      <Routes>
-        <Header />
-      </Routes>
+      <Header />
     </div>
   );
 }
